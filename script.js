10b34c458de8e99e2e5160c50d292ce01d3c653b
adhitya <aji@triyakom.com>
2023-09-01 16:49:09 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+// Typing animation
+var typed = new Typed(".typing", {
+  strings: ['Web Developer', 'Backend Dev', 'Database Admin', 'Freelancer'],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+})
+var typed = new Typed(".typing-2", {
+  strings: ['Web Developer', 'Backend Dev', 'Database Admin', 'Freelancer'],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+})
