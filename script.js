daf7d9d1b39a125b952f337a66bf66e636e8acbc
adhitya <aji@triyakom.com>
2023-09-05 14:14:59 +0700

    Update Main Page

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ function toggleMenu() {
 }
 // Typing animation
 var typed = new Typed(".typing", {
-  strings: ['Web Developer', 'Backend Dev', 'Database Admin', 'Freelancer'],
+  strings: ['Web Developer', 'Backend Dev', 'Database Admin', 'Frontend Dev'],
   typeSpeed: 100,
   backSpeed: 60,
   loop: true,
 })
 var typed = new Typed(".typing-2", {
-  strings: ['Web Developer', 'Backend Dev', 'Database Admin', 'Freelancer'],
+  strings: ['Web Developer', 'Backend Dev', 'Database Admin', 'Frontend Dev'],
   typeSpeed: 100,
   backSpeed: 60,
   loop: true,
